a35d01da163e569a7a65473623a6b0fed063e6c2
speterson25 <speterson25@dtechhs.org>
2025-03-03 10:51:57 -0800

    set up intro and prepared for shop

--- blubblubblub/scripts/1_start.gd
+++ blubblubblub/scripts/1_start.gd
@@ -3,4 +3,4 @@ extends Node2D
 
 
 func _on_texture_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://2FishermanMain.tscn")
+	get_tree().change_scene_to_file("res://openingcutscene.tscn")
